5f010dc58b1c52501eff850f0ce21a19b7ba4b0c
SaikatTalukder7 <saikat60mu@gmail.com>
2026-04-18 00:49:40 +0600

    add Course Details page

---
+++ Frontend/src/pages/CourseDetails.jsx
@@ -0,0 +1,17 @@
+function CourseDetails() {
+  return (
+    <div>
+      <h1>Course Details</h1>
+
+      <h2>Web Development Bootcamp</h2>
+
+      <p>
+    
+      </p>
+
+      <button>Enroll Now</button>
+    </div>
+  );
+}
+
+export default CourseDetails;
